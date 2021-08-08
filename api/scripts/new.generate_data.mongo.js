c7d8eea7016cfbe527ff4f0b8e0cb5934c808b99
An Nguyen <nguyen.an.1196@gmail.com>
2021-08-08 11:45:13 -0700

    Drop Grouping functionality, focus on personal expense first

--- api/scripts/new.generate_data.mongo.js
+++ api/scripts/new.generate_data.mongo.js
@@ -1,13 +1,35 @@
 /* global db print */
 /* eslint no-restricted-globals: "off" */
 
-db.expenses.dropIndex('group_1'); // Bug fixed for now
+// db.expenses.dropIndex('group_1'); // Bug fixed for now
 
-const owners = ['Ravan', 'Eddie', 'Pieta', 'Parvati', 'Victor'];
-const categories = ['Housing', 'Transportation', 'Dinning', 'Groceries', 
-                  'Savings', 'Entertainment', 'UtilitiesAndPhone',
-                  'Medical', 'Clothing', 'Misc' 
-                ];
+const owners = [
+  {
+    name: 'Ravan',
+    email: 'ravan@a'
+  },
+  {
+    name: 'Eddie',
+    email: 'eddie@1'
+  },
+  {
+    name: 'Pieta',
+    email: 'pieta@1'
+  },
+  {
+    name: 'Parvati',
+    email: 'p@a'
+  },
+  {
+    name: 'Victor',
+    email: 'vvv@vvv'
+  } 
+  ];
+
+  const categories = ['Housing', 'Transportation', 'Dinning', 'Groceries', 
+                    'Savings', 'Entertainment', 'UtilitiesAndPhone',
+                    'Medical', 'Clothing', 'Misc' 
+                  ];
 
 const initialCount = db.expenses.count();
 
@@ -17,15 +39,15 @@ for (let i = 0; i < 100; i += 1) {
   const created = new Date(randomCreatedDate);
 
   const owner = owners[Math.floor(Math.random() * 5)];
+  const email = owner.email;
   const category = categories[Math.floor(Math.random() * 10)];
   const description = `Lorem ipsum dolor sit amet, ${i}`;
   const amount = Math.ceil(Math.random() * 20);
   const paid = Math.ceil(Math.random() * 5);
   const id = initialCount + i + 1;
-  const group = { groupName: 'group' + Math.ceil(Math.random() * 20) };
 
   const expense = {
-    id, owner, description, category, created, amount, paid, group
+    id, owner, email, description, category, created, amount, paid
   };
   db.expenses.insertOne(expense);
 }
--- api/scripts/new.init.mongo.js
+++ api/scripts/new.init.mongo.js
@@ -13,31 +13,28 @@ db.users.remove({});
 const expensesDB = [
   {
     id: 1,
-    owner: 'Tony',
+    owner: {
+      signedIn: true,
+      name: 'Tony',
+      email: 'tony@stark.com'
+    },
+    email: 'tony@strak.com',
     title: 'Movie Night',
-    description: '',
+    description: 'Fast and Furious 9',
     category: 'Entertainment',
     created: new Date('2021-07-04'),
     amount: 69.95,
     paid: 15.05,
     imageSrc: 'https://www.google.com/search?q=movie+receipt&tbm=isch&ved=2ahUKEwjf1c2r8pzyAhW7LDQIHSHpAv4Q2-cCegQIABAA&oq=movie+re&gs_lcp=CgNpbWcQARgAMgQIABBDMggIABCABBCxAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgQIIxAnOgsIABCABBCxAxCDAToHCAAQsQMQQ1DOsAFY5rYBYMfBAWgAcAB4AIABSogBjASSAQE4mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=lm0NYd-yNbvZ0PEPodKL8A8&bih=789&biw=1440#imgrc=8sNzkh7W38NBRM',
-    group: {
-      groupName: 'group1',
-      groupMember: [
-        {
-          memberName: 'person1',
-          id: 1234,
-        },
-        {
-          memberName: 'person2',
-          id: 4321,
-        }
-      ]
-    },
   },
   {
     id: 2,
-    owner: 'John',
+    owner: {
+      signedIn: false,
+      name: 'John',
+      email: 'johnny@yahoo.com'
+    },
+    email: 'johnny@yahoo.com',
     title: 'Cheesecake Factory',
     description: 'On a tuesday',
     category: 'Dinning',
@@ -45,15 +42,6 @@ const expensesDB = [
     amount: 105.5,
     paid: 52.25,
     imageSrc: 'https://www.google.com/shttps://www.google.com/search?q=restaurant+receipt&tbm=isch&ved=2ahUKEwiv1tjf9JzyAhUQjp4KHZRBBfEQ2-cCegQIABAA&oq=restaurant+rec&gs_lcp=CgNpbWcQARgAMgUIABCABDIECAAQQzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBAgAEEMyBQgAEIAEMgUIABCABDIFCAAQgAQ6BAgjECc6CAgAEIAEELEDOggIABCxAxCDAToLCAAQgAQQsQMQgwE6BwgAELEDEENQixBY_zZglz5oA3AAeACAAUaIAbsHkgECMTWYAQCgAQGqAQtnd3Mtd2l6LWltZ8ABAQ&sclient=img&ei=HHANYa_YO5Cc-gSUg5WIDw&bih=789&biw=1440#imgrc=lwxf-xfKw80d8Mearch?q=movie+receipt&tbm=isch&ved=2ahUKEwjf1c2r8pzyAhW7LDQIHSHpAv4Q2-cCegQIABAA&oq=movie+re&gs_lcp=CgNpbWcQARgAMgQIABBDMggIABCABBCxAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEOgQIIxAnOgsIABCABBCxAxCDAToHCAAQsQMQQ1DOsAFY5rYBYMfBAWgAcAB4AIABSogBjASSAQE4mAEAoAEBqgELZ3dzLXdpei1pbWfAAQE&sclient=img&ei=lm0NYd-yNbvZ0PEPodKL8A8&bih=789&biw=1440#imgrc=8sNzkh7W38NBRM',
-    group: {
-      groupName: 'group2',
-      groupMember: [
-        {
-          memberName: 'person3',
-          id: 3333,
-        }
-      ]
-    },
   },
 ];
 
@@ -65,7 +53,7 @@ db.counters.remove({ _id: 'expenses' });
 db.counters.insert({ _id: 'expenses', current: count });
 
 db.expenses.createIndex({ id: 1 }, { unique: true });
-db.expenses.createIndex({ group: 1}, { unique: true });
+db.expenses.createIndex({ email: 1 });
 db.expenses.createIndex({ category: 1 });
 db.expenses.createIndex({ owner: 1 });
 db.expenses.createIndex({ created: 1 });
