75750178b94e46178c81ca32de5774e761d90f37
nguyenan11 <nguyen.an.1196@gmail.com>
2021-08-16 17:57:43 -0700

    Add Carousel for home page

--- ui/src/Home.jsx
+++ ui/src/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Jumbotron, Carousel } from 'react-bootstrap';
 import UserContext from './UserContext.js';
 import store from './store.js';
 import graphQLFetch from './graphQLFetch.js';
@@ -32,11 +32,35 @@ export default class Home extends React.Component {
 
   render() {
     const { apiHome } = this.state;
-    return (
+    return (<>
       <Jumbotron>
         <h1 id="welcome-msg" data-text={apiHome}>{apiHome}</h1>
       </Jumbotron>
-    );
+      
+      <Carousel>
+        <Carousel.Item>
+          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/bill.png" />
+          <h3>Track your spending</h3>
+          <p>Keep track of all your expenses, see what you've been paying</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/category.png" />
+          <h3>Organize expenses</h3>
+          <p>Categorize your expense with details: Housing, Transportation, Dining, Groceries, Savings,... and more</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/add.png" />
+          <h3>Add expenses easily</h3>
+          <p>Quickly add expenses on the go before you forget</p>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/summary.png" />
+          <h3>Be the boss of your finance</h3>
+          <p>Visualize your money flow with our comprehensive summary</p>
+        </Carousel.Item>
+      </Carousel>
+
+    </>);
   }
 }
 
