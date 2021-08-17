b7b889d8e777d0bd610a449a71dd59ffaf72f33e
Phong <hoang.ph@northeastern.edu>
2021-08-16 22:22:52 -0700

    fix eslint and edit UI for home page

--- ui/src/Home.jsx
+++ ui/src/Home.jsx
@@ -32,35 +32,35 @@ export default class Home extends React.Component {
 
   render() {
     const { apiHome } = this.state;
-    return (<>
-      <Jumbotron>
-        <h1 id="welcome-msg" data-text={apiHome}>{apiHome}</h1>
-      </Jumbotron>
-      
-      <Carousel>
-        <Carousel.Item>
-          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/bill.png" />
-          <h2>Track your spending</h2>
-          <h3>Keep track of all your expenses, see what you've been paying</h3>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/category.png" />
-          <h2>Organize expenses</h2>
-          <h3>Categorize your expense with details: Housing, Transportation, Dining, Groceries, Savings,... and more</h3>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/add.png" />
-          <h2>Add expenses easily</h2>
-          <h3>Quickly add expenses on the go before you forget</h3>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/summary.png" />
-          <h2>Be the boss of your finance</h2>
-          <h3>Visualize your money flow with our comprehensive summary</h3>
-        </Carousel.Item>
-      </Carousel>
-
-    </>);
+    return (
+      <>
+        <Jumbotron>
+          <h1 id="welcome-msg" data-text={apiHome}>{apiHome}</h1>
+        </Jumbotron>
+        <Carousel>
+          <Carousel.Item>
+            <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/bill.png" />
+            <h2>Track your spending</h2>
+            <h3>Keep track of all your expenses, see what you've been paying</h3>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/category.png" />
+            <h2>Organize expenses</h2>
+            <h3>Categorize your expense with details: Housing, Transportation, Dining, Groceries, Savings,... and more</h3>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/add.png" />
+            <h2>Add expenses easily</h2>
+            <h3>Quickly add expenses on the go before you forget</h3>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="img-responsive center-block" alt="Dollar sign" src="../styles/icons/summary.png" />
+            <h2>Be the boss of your finance</h2>
+            <h3>Visualize your money flow with our comprehensive summary</h3>
+          </Carousel.Item>
+        </Carousel>
+      </>
+    );
   }
 }
 
